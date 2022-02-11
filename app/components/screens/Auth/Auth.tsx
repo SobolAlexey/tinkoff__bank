9f228148786727db7b59cc8e40b1ec031461d981
Alexey <metallica2000@list.ru>
2022-02-11 12:39:56 +0500

    create Auth.tsx

--- app/components/screens/Auth/Auth.tsx
+++ app/components/screens/Auth/Auth.tsx
@@ -1,12 +1,14 @@
 import { View, Text } from 'react-native'
 import React, {FC} from 'react'
 import { styleCenter } from '../../layout/Layout'
-
+import tw from "tailwind-react-native-classnames"
 
 const Auth:FC = () => {
   return (
     <View style={styleCenter}>
+      <View style={tw`mx-5 justify-center items-center`}> 
       <Text>Auth</Text>
+      </View>
     </View>
   )
 }
